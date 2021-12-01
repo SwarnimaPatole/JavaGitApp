b78f3e3cc674dd5822b7975ee2dc38112f5943e0
SwarnimaPatole <swarnimapatole@gmail.com>
2021-12-01 16:14:30 +0530

    1 Commit

---
+++ NewGitRepos/src/main/java/com/lti/gitdemos/ClientApp.java
@@ -0,0 +1,12 @@
+package com.lti.gitdemos;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+    Product p1=new Product(101,"Mobile");
+		
+		System.out.println(p1);
+
+	}
+
+}
---
+++ NewGitRepos/src/main/java/com/lti/gitdemos/Product.java
@@ -0,0 +1,11 @@
+package com.lti.gitdemos;
+
+public class Product {
+	private int prdId;
+	private String prdName;
+	public Product(int prdId, String prdName) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+}
+}
