d8131f9a78d43207a3c3a78ee446a143a005d13a
SwarnimaPatole <swarnimapatole@gmail.com>
2021-12-01 14:44:16 +0530

    1st commit

---
+++ JavaGitApp/src/main/java/com/lti/gitdemos/ClientApp.java
@@ -0,0 +1,13 @@
+package com.lti.gitdemos;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		
+		Product p1=new Product(101,"Mobile");
+		
+		System.out.println(p1);
+
+	}
+
+}
